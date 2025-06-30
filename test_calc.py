6def61061e5e71e3011de94fe09ad3438715767a
Bryan Carrillo <carrbryan0123@gmail.com>
2025-06-30 17:25:51 +0000

    fixed unit test indentation

--- test_calc.py
+++ test_calc.py
@@ -15,8 +15,8 @@ class TestCalc(unittest.TestCase):
     def test_mul(self):
         self.assertEqual(self.calc.mul(2, 3), 6)
     def test_div(self):
-	self.assertEqual(self.calc.div(6,3),2)
+	      self.assertEqual(self.calc.div(6,3),2)
     def test_pow(self):
-	self.assertEqual(self.calc.pow(2,3),8)
+	      self.assertEqual(self.calc.pow(2,3),8)
     def test_sqrt(self):
-	self.assertEqual(self.calc.sqrt(25),5)
+	      self.assertEqual(self.calc.sqrt(25),5)
